5adf8f2271867038dc56ebe87a411cbac81821f9
superzbo <zbo2@qq.com>
2015-05-04 23:02:16 +0800

    encap hrml output

---
+++ src/main/java/stock/meta/MatchedStock.java
@@ -0,0 +1,87 @@
+package stock.meta;
+
+public class MatchedStock {
+    private String qfiiThisQ;
+    private String sbThisQ;
+    private String qfiiLastQ;
+    private String sbLastQ;
+    private String imageMpnth;
+    private String imageWeek;
+    private String imageDay;
+    private Stock sotck;
+    private MatchType matchType;
+
+    public String getSbThisQ() {
+        return sbThisQ;
+    }
+
+    public void setSbThisQ(String sbThisQ) {
+        this.sbThisQ = sbThisQ;
+    }
+
+    public Stock getSotck() {
+        return sotck;
+    }
+
+    public void setSotck(Stock sotck) {
+        this.sotck = sotck;
+    }
+
+    public String getQfiiThisQ() {
+        return qfiiThisQ;
+    }
+
+    public void setQfiiThisQ(String qfiiThisQ) {
+        this.qfiiThisQ = qfiiThisQ;
+    }
+
+    public MatchType getMatchType() {
+        return matchType;
+    }
+
+    public void setMatchType(MatchType matchType) {
+        this.matchType = matchType;
+    }
+
+    public String getSbLastQ() {
+        return sbLastQ;
+    }
+
+    public void setSbLastQ(String sbLastQ) {
+        this.sbLastQ = sbLastQ;
+    }
+
+    public String getQfiiLastQ() {
+        return qfiiLastQ;
+    }
+
+    public void setQfiiLastQ(String qfiiLastQ) {
+        this.qfiiLastQ = qfiiLastQ;
+    }
+
+    public String getImageMpnth() {
+        return imageMpnth;
+    }
+
+    public void setImageMpnth(String imageMpnth) {
+        this.imageMpnth = imageMpnth;
+    }
+
+    public String getImageWeek() {
+        return imageWeek;
+    }
+
+    public void setImageWeek(String imageWeek) {
+        this.imageWeek = imageWeek;
+    }
+
+    public String getImageDay() {
+        return imageDay;
+    }
+
+    public void setImageDay(String imageDay) {
+        this.imageDay = imageDay;
+    }
+
+    enum MatchType {SB, QFII, BOTH}
+}
