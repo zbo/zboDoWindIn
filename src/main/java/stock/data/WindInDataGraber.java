7fafe97a03229ed7d505c77b67c4b0c927dbba99
superzbo <zbo2@qq.com>
2015-05-06 11:19:39 +0800

    get last quarter data

--- src/main/java/stock/data/WindInDataGraber.java
+++ src/main/java/stock/data/WindInDataGraber.java
@@ -24,17 +24,16 @@ import static java.lang.String.format;
  */
 public class WindInDataGraber {
     AbstractMarket market;
-    private org.openqa.selenium.WebDriver WebDriver;
-
-    @Inject
-    private Finance finance;
     @Inject
     Gson gson;
     @Inject
     WindInDataRecorder recorder;
-
-    private Hashtable<String, Stock> history = new Hashtable<>();
     WebDriver webDriver;
+    private org.openqa.selenium.WebDriver WebDriver;
+    @Inject
+    private Finance finance;
+    private Hashtable<String, Stock> history = new Hashtable<>();
+    private String contentFormat="%s - %s";
 
     public void grab(String classname) throws ClassNotFoundException, IllegalAccessException, InstantiationException, IOException {
         market = (AbstractMarket) (Class.forName(classname).newInstance());
@@ -49,8 +48,8 @@ public class WindInDataGraber {
                 MatchedStock matchedStock = new MatchedStock();
                 matchThisQuarterFromWeb(stock, matchedStock);
                 if (matchedStock.thisQmatched()) {
-                    matchLastQuarterFromWeb(stock,matchedStock);
-                    generateSinaImages(stock,matchedStock);
+                    matchLastQuarterFromWeb(stock, matchedStock);
+                    generateSinaImages(stock, matchedStock);
                     this.recorder.record(matchedStock);
                 }
             }
@@ -63,13 +62,29 @@ public class WindInDataGraber {
     }
 
     private void matchLastQuarterFromWeb(Stock stock, MatchedStock matchedStock) {
-
+        try {
+            WebElement link2 = webDriver.findElement(By.id("report2"));
+            link2.click();
+            WebElement div2 = webDriver.findElement(By.id("MainHolder_Sumary2"));
+            String date = getDate();
+            List<WebElement> trs = div2.findElements(By.tagName("tr"));
+            for (WebElement tr : trs) {
+                String content = getStringLine(tr).toString();
+                matchedStock.setSotck(stock);
+                if (finance.IsSB(content)) {
+                    matchedStock.setSbLastQ(format(contentFormat, date, content));
+                } else if (finance.IsQfii(content)) {
+                    matchedStock.setQfiiLastQ(format(contentFormat, date, content));
+                }
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("error one" + stock.getName());
+        }
     }
 
     private void matchThisQuarterFromWeb(Stock stock, MatchedStock matchedStock) throws IOException {
         webDriver.get(format(WindIn.getSource_base(), stock.getCode().substring(2), stock.getCode().substring(0, 2)));
-        String date=getDate();
-        String contentFormat="%s - %s";
+        String date = getDate();
         try {
             WebElement div = webDriver.findElement(By.id("MainHolder_Sumary1"));
             List<WebElement> trs = div.findElements(By.tagName("tr"));
@@ -77,10 +92,9 @@ public class WindInDataGraber {
                 String content = getStringLine(tr).toString();
                 matchedStock.setSotck(stock);
                 if (finance.IsSB(content)) {
-                    matchedStock.setSbThisQ(format(contentFormat,date,content));
-                }
-                else if(finance.IsQfii(content)){
-                    matchedStock.setQfiiThisQ(format(contentFormat,date,content));
+                    matchedStock.setSbThisQ(format(contentFormat, date, content));
+                } else if (finance.IsQfii(content)) {
+                    matchedStock.setQfiiThisQ(format(contentFormat, date, content));
                 }
             }
         } catch (NoSuchElementException e) {
